b5a2aa8525276ef4ac3884d449dbabdd3be06913
pblop <pablo@pabl.eu>
2025-01-10 02:36:08 +0100

    feat: add python script to convert from .s19 to a binary format, add program folder

---
+++ scripts/s19reader.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env uv run
+
+import sys
+
+with open(sys.argv[1], 'r', encoding="ASCII") as inf:
+  with open(sys.argv[2], "wb") as outf:
+    for line in inf.readlines():
+      line = line.strip()
+
+      recordtype = line[:2]
+      bytecount = int(line[2:4], 16)
+      address = int(line[4:8], 16)
+      data = line[8:-2]
+      checksum = line[-2:]
+    
+      match recordtype:
+        case "S0":
+          print(f"S0 (Header): {data}")
+        case "S1":
+          print(f"S1 (Data): {data}")
+          outf.seek(address, 0) # seek_set=0
+          outf.write(bytes.fromhex(data))
+        case "S9":
+          print(f"S9 (Termination): {data}")
+
+
