05a2cc8f7d3398c5ee8931057fb310671a1f1e4b
pblop <pablo@pabl.eu>
2025-01-15 16:21:31 +0100

    feat: add stop module
    note: currently the ui clock module is not updated when the stop module
    pauses the system.
    for that, it would be useful to provide a better way for modules to know
    who sent a message (this could be implemented as sender/receiver
    arguments in the send functions)

---
+++ src/m6809/extra/stop.ts
@@ -0,0 +1,51 @@
+import type { IModule } from "../../types/module";
+import type { ISimulator } from "../../types/simulator";
+import type { TypedEventTransceiver, EventDeclaration } from "../../types/event";
+import { element } from "../../utils.js";
+
+type StopConfig = {
+  address: number;
+};
+
+function validateScreenConfig(config: Record<string, unknown>): StopConfig {
+  if (typeof config.address !== "number") throw new Error("[Stop] address must be a number");
+
+  return config as StopConfig;
+}
+
+class Stop implements IModule {
+  id: string;
+  event_transceiver: TypedEventTransceiver;
+  config: StopConfig;
+
+  getEventDeclaration(): EventDeclaration {
+    return {
+      provided: ["ui:clock:pause"],
+      required: {
+        "memory:write": this.onMemoryWrite,
+      },
+      optional: {},
+    };
+  }
+
+  constructor(
+    id: string,
+    config: Record<string, unknown>,
+    eventTransceiver: TypedEventTransceiver,
+  ) {
+    this.id = id;
+    this.event_transceiver = eventTransceiver;
+
+    this.config = validateScreenConfig(config);
+
+    console.log(`[${this.id}] Module initialized.`);
+  }
+
+  onMemoryWrite = (address: number, data: number): void => {
+    if (address !== this.config.address) return;
+
+    this.event_transceiver.emit("ui:clock:pause");
+  };
+}
+
+export default Stop;
