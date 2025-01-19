590050cb1f6489645aec23fa2a970b243aae9376
pblop <pablo@pabl.eu>
2025-01-19 03:43:48 +0100

    feat: implemented mirror registers

--- src/m6809/ui/register_ui.ts
+++ src/m6809/ui/register_ui.ts
@@ -1,12 +1,15 @@
 import type { IModule } from "../../types/module.js";
 import type { ISimulator } from "../../types/simulator.js";
 import type { EventDeclaration, TypedEventTransceiver } from "../../types/event.js";
-import { element } from "../../utils.js";
+import { element, isNumber, parseNumber } from "../../utils.js";
 
 type RegisterInfoConfig = {
   size: number;
   pointer?: boolean;
-  mirror?: string;
+  mirror?: {
+    register: string;
+    mask: number;
+  };
 };
 type RegisterUIConfig = {
   registers: Record<string, RegisterInfoConfig>;
@@ -17,8 +20,14 @@ function validateRegisterInfo(registerInfo: Record<string, unknown>): RegisterIn
     throw new Error("[RegisterUI] Register size must be a number");
   if (registerInfo.pointer && typeof registerInfo.pointer !== "boolean")
     throw new Error("[RegisterUI] Register pointer must be a boolean");
-  if (registerInfo.mirror && typeof registerInfo.mirror !== "string")
-    throw new Error("[RegisterUI] Register mirror must be a string");
+  if (registerInfo.mirror && typeof registerInfo.mirror === "object") {
+    if (!("register" in registerInfo.mirror) || typeof registerInfo.mirror.register !== "string")
+      throw new Error("[RegisterUI] Mirror register must be a string");
+    if (!("mask" in registerInfo.mirror) || !isNumber(registerInfo.mirror.mask))
+      throw new Error("[RegisterUI] Mirror mask must be a number (or a hex representation)");
+    if (typeof registerInfo.mirror.mask === "string")
+      registerInfo.mirror.mask = parseNumber(registerInfo.mirror.mask);
+  }
 
   return registerInfo as RegisterInfoConfig;
 }
@@ -34,6 +43,17 @@ function validateRegisterUIConfig(config: Record<string, unknown>): RegisterUICo
   );
 
   return config as RegisterUIConfig;
+}
+
+function indexOfLsb(mask: number): number {
+  let count = 0;
+  // I shift a 1 bit to the left until I find the first 1 in the mask, that's
+  // the number of bits I need to shift to the right to get the first 1 in the
+  // mask to the least significant bit.
+  while ((mask & (1 << count)) === 0) {
+    count++;
+  }
+  return count;
 }
 
 class RegisterUI implements IModule {
@@ -85,6 +105,27 @@ class RegisterUI implements IModule {
 
     this.registerValues[register] = value;
     cell.textContent = this.formatRegister(register, value);
+
+    // NOTE: this could be optimized by keeping a list of mirrors for each register,
+    // and the number of bits to shift, instead of iterating over all registers every time,
+    // and calculating the number of bits to shift every time (which is constant for a mask).
+    // But this is fine for now, since this is only done when a register is updated.
+
+    // Update registers that mirror this one.
+    const mirrors = Object.entries(this.config.registers).filter(
+      ([_, { mirror }]) => mirror?.register === register,
+    );
+
+    for (const [name, { mirror }] of mirrors) {
+      const mirroredCell = this.panel.querySelector(`.register-${name}`);
+      if (!mirroredCell) throw new Error(`[${this.id}] Missing cell for register ${name}`);
+
+      // biome-ignore lint/style/noNonNullAssertion: I've already checked for the mirror property's existence above.
+      const mask = mirror!.mask;
+      const mirroredValue = (value & mask) >> indexOfLsb(mask);
+      this.registerValues[name] = mirroredValue;
+      mirroredCell.textContent = this.formatRegister(name, mirroredValue);
+    }
   };
 
   formatRegister(register: string, data: number): string {
