4cce015219c5d312a099a446deb5bba79d243969
pblop <pablo@pabl.eu>
2025-01-23 14:42:31 +0100

    feat: show commit message on deployment info (not escaped)
    this is a security risk, but for now, I can deal with it

--- src/m6809/ui/gui.ts
+++ src/m6809/ui/gui.ts
@@ -81,6 +81,7 @@ class Gui implements IModule {
           properties: {
             className: "deployment-info",
             innerText: `${info.commit.slice(0, 7)} (${timeAgo(date)})`,
+            title: info.message,
           },
         });
         this.root_element.appendChild(deployment_info_element);
