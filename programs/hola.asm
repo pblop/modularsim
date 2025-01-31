7e886688fd52705df9b2c7c84c41b1d1531ae927
pblop <pablo@pabl.eu>
2025-02-01 00:02:41 +0100

    build: add program build step and store assembly files

---
+++ programs/hola.asm
@@ -0,0 +1,21 @@
+; Programa de ejemplo: hola.asm
+
+        .area PROG (ABS)
+
+        .org 0x100
+cadena: .ascii "Hola, mundo."
+        .byte   10      ; 10 es CTRL-J: salto de lInea
+        .byte   0       ; 0 es CTRL-@: fin de cadena
+
+        .globl programa
+programa:
+        ldx #cadena
+bucle:  lda ,x+
+        beq acabar
+        sta 0xFF00      ; salida por pantalla
+        bra bucle
+acabar: clra
+        sta 0xFF01
+
+        .org 0xFFFE     ; Vector de RESET
+        .word programa
