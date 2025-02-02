c820097d66a411d135c362f3794bafe4ef197c60
pblop <pablo@pabl.eu>
2025-02-02 19:17:47 +0100

    scripts: add commit body to deployment info
    (something in the body to check if it works correctly)

--- scripts/generate-deployment-info.sh
+++ scripts/generate-deployment-info.sh
@@ -1,1 +1,1 @@
-git log -1 --pretty=format:'%H%x00%aN <%aE>%x00%ad%x00%s' | jq --slurp --raw-input 'split("\u0000") as $arr | {commit: $arr[0], author: $arr[1], date: $arr[2], message: $arr[3]}'
+git log -1 --pretty=format:'%H%x00%aN <%aE>%x00%ad%x00%s%x00%b' | jq --slurp --raw-input 'split("\u0000") as $arr | {commit: $arr[0], author: $arr[1], date: $arr[2], message: $arr[3], body: $arr[4]}'
