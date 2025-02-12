429e1747c9fa3c5a484da6a62b46e898116d1aef
pblop <pablo@pabl.eu>
2025-02-12 23:10:07 +0100

    docs: info about event declaration stuff

--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -1,6 +1,13 @@
 import type { EventDeclaration, TypedEventTransceiver } from "./event";
 
 export interface IModule {
+  /**
+   * This field is not static, and as such, will be called after the module is
+   * initialized through the constructor. That means that the module can use its
+   * variables and config to set up different event listeners.
+   * It will also only be called once, after the module is initialized, at the
+   * start of the simulation.
+   */
   getEventDeclaration(): EventDeclaration;
 }
 
