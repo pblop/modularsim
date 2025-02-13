fec89ce1339ef6ceeafffaac5df0c875fc95927d
pblop <pablo@pabl.eu>
2025-02-14 00:06:37 +0100

    feat: add multiplexer, and multiplexer support for memory and memory:bulk:write event

--- src/m6809/hardware/memory.ts
+++ src/m6809/hardware/memory.ts
@@ -4,43 +4,37 @@ import type {
   SimulationModuleInteraction,
 } from "../../types/module.js";
 import type { ISimulator } from "../../types/simulator.js";
-import type { EventContext, EventDeclaration, TypedEventTransceiver } from "../../types/event.js";
+import type {
+  EventBaseName,
+  EventCallback,
+  EventContext,
+  EventDeclaration,
+  EventDeclarationListeners,
+  EventName,
+  EventParams,
+  TypedEventTransceiver,
+} from "../../types/event.js";
+import { verify } from "../../general/config.js";
+import { joinEventName } from "../../general/event.js";
 
 type MemoryType = "ram" | "rom";
 type MemoryConfig = {
-  start: number;
   size: number;
   type: MemoryType;
-  read_delay?: number;
-  write_delay?: number;
+  multiplexer?: string;
 };
-
-function validate_memory_config(config: Record<string, unknown>): MemoryConfig {
-  // Check that the properties have the correct types.
-  if (typeof config.start !== "number") throw new Error("[Memory] start must be a number");
-  if (typeof config.size !== "number") throw new Error("[Memory] size must be a number");
-  if (typeof config.type !== "string" || !["ram", "rom"].includes(config.type))
-    throw new Error(`[Memory] type must be 'ram' or 'rom'`);
-  if (config.read_delay && typeof config.read_delay !== "number")
-    throw new Error("[Memory] read_delay must be a number");
-  if (config.write_delay && typeof config.write_delay !== "number")
-    throw new Error("[Memory] write_delay must be a number");
-
-  // Now that we know that the properties are there, we can safely cast the config.
-  return config as MemoryConfig;
-}
 
 class Memory implements IModule {
   simulation: SimulationModuleInteraction;
   id: string;
 
+  config: MemoryConfig;
+
   memory: Uint8Array;
-  start: number;
-  type: MemoryType;
-  read_delay: number;
-  write_delay: number;
 
   getModuleDeclaration(): ModuleDeclaration {
+    const multiplexedName = (event: EventBaseName) =>
+      this.config.multiplexer ? joinEventName(event, this.id) : event;
     return {
       events: {
         provided: [
@@ -52,11 +46,11 @@ class Memory implements IModule {
         ],
         required: {},
         optional: {
-          "memory:read": this.onMemoryRead,
-          "memory:write": this.onMemoryWrite,
-          "ui:memory:read": this.onUiMemoryRead,
-          "ui:memory:write": this.onUiMemoryWrite,
-          "ui:memory:bulk:write": this.onUiMemoryBulkWrite,
+          [multiplexedName("memory:read")]: this.onMemoryRead,
+          [multiplexedName("memory:write")]: this.onMemoryWrite,
+          [multiplexedName("ui:memory:read")]: this.onUiMemoryRead,
+          [multiplexedName("ui:memory:write")]: this.onUiMemoryWrite,
+          [multiplexedName("ui:memory:bulk:write")]: this.onUiMemoryBulkWrite,
         },
       },
     };
@@ -74,63 +68,59 @@ class Memory implements IModule {
     console.log(`[${this.id}] Initializing module.`);
 
     // Verify that configuration is ok.
-    if (!config) throw new Error(`[${this.id}] No configuration provided`);
+    this.config = verify<MemoryConfig>(
+      config,
+      {
+        size: { type: "number", required: true },
+        type: { type: "string", required: true, enum: ["ram", "rom"] },
+        multiplexer: { type: "string", required: false },
+      },
+      this.id,
+    );
 
-    const parsed_config = validate_memory_config(config);
-    const { size, start, type } = parsed_config;
+    this.memory = new Uint8Array(this.config.size);
 
-    this.memory = new Uint8Array(size);
-    this.start = start;
-    this.type = type;
-    this.read_delay = parsed_config.read_delay ?? 95;
-    this.write_delay = parsed_config.write_delay ?? 95;
-
-    console.log(`[${this.id}] Initialized ${type} memory at ${start} with size ${size}`);
+    console.log(`[${this.id}] Initialized ${this.config.type} memory of size ${this.config.size}`);
   }
 
+  emitMultiplexed<B extends EventBaseName>(event: B, ...args: EventParams<B>): void {
+    if (this.config.multiplexer) {
+      this.simulation.emit(joinEventName(event, this.config.multiplexer), ...args);
+    } else {
+      this.simulation.emit(event, ...args);
+    }
+  }
   onUiMemoryRead = (address: number, ctx: EventContext): void => {
-    // If the address is out of bounds, do nothing.
-    if (address < this.start || address >= this.start + this.memory.length) return;
-
-    const data = this.memory[address - this.start];
+    const data = this.memory[address];
     this.simulation.emit("ui:memory:read:result", address, data);
   };
   onUiMemoryWrite = (address: number, data: number, ctx: EventContext): void => {
-    // If the address is out of bounds, do nothing.
-    if (address < this.start || address >= this.start + this.memory.length) return;
-
-    this.memory[address - this.start] = data;
+    this.memory[address] = data;
     this.simulation.emit("ui:memory:write:result", address, data);
   };
-  onUiMemoryBulkWrite = (data: Uint8Array): void => {
+  onUiMemoryBulkWrite = (address: number, data: Uint8Array): void => {
     if (data.length > this.memory.length) {
       throw new Error(`[${this.id}] Attempted to write more data than memory size.`);
     }
 
     this.memory.set(data);
-    this.simulation.emit("ui:memory:bulk:write:result", data);
+    this.simulation.emit("ui:memory:bulk:write:result", address, data);
   };
   onMemoryRead = (address: number) => {
-    // If the address is out of bounds, do nothing.
-    if (address < this.start || address >= this.start + this.memory.length) return;
-
-    const data = this.memory[address - this.start];
+    const data = this.memory[address];
     this.simulation.onceCycle(
       () => {
-        this.simulation.emit("memory:read:result", address, data);
+        this.emitMultiplexed("memory:read:result", address, data);
       },
       { order: -1 },
     );
   };
   onMemoryWrite = (address: number, data: number) => {
-    // If the address is out of bounds, do nothing.
-    if (address < this.start || address >= this.start + this.memory.length) return;
-
-    this.memory[address - this.start] = data;
+    this.memory[address] = data;
 
     this.simulation.onceCycle(
       () => {
-        this.simulation.emit("memory:write:result", address, data);
+        this.emitMultiplexed("memory:write:result", address, data);
       },
       { order: -1 },
     );
---
+++ src/m6809/hardware/multiplexer.ts
@@ -0,0 +1,155 @@
+import type { IModule, ModuleDeclaration } from "../../types/module.js";
+import type { ISimulator } from "../../types/simulator.js";
+import type {
+  EventBaseName,
+  EventCallbackArgs,
+  EventContext,
+  EventDeclaration,
+  EventDeclarationListeners,
+  EventName,
+  EventParams,
+  TypedEventTransceiver,
+} from "../../types/event.js";
+import { verify } from "../../general/config.js";
+import { joinEventName, separateEventName } from "../../general/event.js";
+
+type MultiplexerEntry = {
+  module: string;
+  start: number;
+  size: number;
+  priority: number;
+};
+type MultiplexerConfig = {
+  entries: MultiplexerEntry[];
+};
+
+// IMPORTANT: These events' first argument is always the address.
+const IncomingEvents: EventBaseName[] = [
+  "memory:read",
+  "memory:write",
+  "ui:memory:read",
+  "ui:memory:write",
+  "ui:memory:bulk:write",
+];
+type IncomingEvents = (typeof IncomingEvents)[number];
+const OutgoingEvents: EventBaseName[] = [
+  "memory:read:result",
+  "memory:write:result",
+  "ui:memory:read:result",
+  "ui:memory:write:result",
+  "ui:memory:bulk:write:result",
+];
+type OutgoingEvents = (typeof OutgoingEvents)[number];
+
+class Multiplexer implements IModule {
+  et: TypedEventTransceiver;
+  id: string;
+
+  config: MultiplexerConfig;
+
+  getModuleDeclaration(): ModuleDeclaration {
+    const incomingEventHandlers = IncomingEvents.map((event) => [event, this.passEventDown(event)]);
+    const outgoingEventHandlers = OutgoingEvents.map((event) => [
+      joinEventName(event, this.id),
+      this.passEventUp(event),
+    ]);
+
+    return {
+      events: {
+        provided: [
+          "memory:read",
+          "memory:write",
+          "ui:memory:read",
+          "ui:memory:write",
+          "ui:memory:bulk:write",
+          "memory:read:result",
+          "memory:write:result",
+          "ui:memory:read:result",
+          "ui:memory:write:result",
+          "ui:memory:bulk:write:result",
+        ],
+        required: {},
+        optional: Object.fromEntries([...incomingEventHandlers, ...outgoingEventHandlers]),
+      },
+    };
+  }
+
+  constructor(
+    id: string,
+    config: Record<string, unknown> | undefined,
+    eventTransceiver: TypedEventTransceiver,
+  ) {
+    // We use the simulator to emit/receive events.
+    this.et = eventTransceiver;
+    this.id = id;
+
+    this.config = verify<MultiplexerConfig>(
+      config,
+      {
+        entries: {
+          type: "array",
+          schema: {
+            type: "object",
+            properties: {
+              module: { type: "string", required: true },
+              start: { type: "number", required: true },
+              size: { type: "number", required: true },
+              priority: { type: "number", default: 0 },
+            },
+          },
+        },
+      },
+      this.id,
+    );
+
+    console.log(`[${this.id}] Initializing Multiplexer module.`);
+  }
+
+  getCorrespondingEntry = (address: number): MultiplexerEntry | undefined => {
+    const entries = this.config.entries
+      .filter((e) => e.start <= address && e.start + e.size > address)
+      .sort((a, b) => a.priority - b.priority);
+    if (entries.length === 0) {
+      console.error(`[${this.id}] No module found for address 0x${address.toString(16)}`);
+      return;
+    }
+    return entries[0];
+  };
+
+  passEventDown<I extends IncomingEvents>(
+    event: EventName<I>,
+  ): (...args: EventCallbackArgs<I>) => void {
+    const [base, _] = separateEventName(event);
+    return (...args: EventCallbackArgs<I>) => {
+      // The first argument is always the address (for the incoming events we've set).
+      const address = args[0] as number;
+      const entry = this.getCorrespondingEntry(address);
+      if (!entry) return;
+
+      // Calculate the address relative to the module, and pass it as the first argument to the
+      // multiplexed module.
+      (args[0] as number) = address - entry.start; // (casted to number to avoid TS error, it's fine)
+
+      // We remove the last argument (context) before passing the args
+      const eventArgs = args.slice(0, -1) as EventParams<I>;
+      this.et.emit(joinEventName(base, entry.module), ...eventArgs);
+    };
+  }
+  passEventUp<O extends OutgoingEvents>(
+    event: EventName<O>,
+  ): (...args: EventCallbackArgs<O>) => void {
+    const [base, _] = separateEventName(event);
+    return (...args: EventCallbackArgs<O>) => {
+      // The first argument is always the address (for the incoming events we've set).
+      const address = args[0] as number;
+      const entry = this.getCorrespondingEntry(address);
+      if (!entry) return;
+
+      // We remove the last argument (context) before passing the args
+      const eventArgs = args.slice(0, -1) as EventParams<O>;
+      this.et.emit(base, ...eventArgs);
+    };
+  }
+}
+
+export default Multiplexer;
