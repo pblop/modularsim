a636c4414afbc8ca6c0db795032ef2bff381ee4a
pblop <pablo@pabl.eu>
2025-02-21 20:53:04 +0100

    docs(cpu/fail): explain why we use the catch-all CpuState in fail.ts instead of "fail"

--- src/m6809/hardware/states/fail.ts
+++ src/m6809/hardware/states/fail.ts
@@ -1,6 +1,13 @@
 import { INSTRUCTIONS } from "../../util/instructions.js";
 import type { CycleStartFn, CycleEndFn, CpuState } from "../../util/state_machine";
 
+// You might have noticed that all the other states' CycleStartFn and CycleEndFn
+// functions use the correct type parameter for the state they're in, and this
+// one instead opts for the generic CpuState type.  This is because it's useful
+// to be able to use this fail state in place of a correct implementation of
+// another state, for preventign typing errors, when implementing the other
+// states.
+
 const start: CycleStartFn<CpuState> = ({ et }, _) => {
   et.emit("cpu:fail");
 };
