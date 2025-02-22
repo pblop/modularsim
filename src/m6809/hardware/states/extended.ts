e351a6aeac68702c1567c5c1c0f9d3cde4065490
pblop <pablo@pabl.eu>
2025-02-22 13:57:11 +0100

    docs(cpu/extended): mark problem with state machine impl

--- src/m6809/hardware/states/extended.ts
+++ src/m6809/hardware/states/extended.ts
@@ -1,9 +1,11 @@
 import type { CycleStartFn, CycleEndFn } from "../../util/state_machine.js";
 
-const start: CycleStartFn<"extended"> = ({ memoryPending, queryMemoryRead }, { ticksOnState }) => {
-  if (memoryPending) return;
-
+const start: CycleStartFn<"extended"> = (
+  { memoryPending, queryMemoryRead },
+  { ctx, ticksOnState },
+) => {
   if (ticksOnState === 0) {
+    ctx.remainingTicks = 1;
     // Fetch the extended address.
     queryMemoryRead("pc", 2);
   }
@@ -14,6 +16,14 @@ const end: CycleEndFn<"extended"> = ({ memoryPending, cpu }, { ctx }) => {
 
   const address = cpu.memoryAction!.valueRead;
   cpu.addressing = { mode: "extended", address };
+
+  // Because of how the state machine is set up, if I add this extra tick,
+  // the extended sta takes 1 more tick than it should.
+  // If I remove it, the extended ldx takes 1 less tick than it should.
+  if (ctx.remainingTicks !== 0) {
+    ctx.remainingTicks--;
+    return null;
+  }
 
   return "execute";
 };
