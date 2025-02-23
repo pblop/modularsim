fa31ca3b56778693e2af7685a0e8eccbe9f68a0b
pblop <pablo@pabl.eu>
2025-02-23 22:01:12 +0100

    docs(extended): there wasn’t a problem with the state machine

--- src/m6809/hardware/states/extended.ts
+++ src/m6809/hardware/states/extended.ts
@@ -17,9 +17,6 @@ const end: CycleEndFn<"extended"> = ({ memoryPending, cpu }, { ctx }) => {
   const address = cpu.memoryAction!.valueRead;
   cpu.addressing = { mode: "extended", address };
 
-  // Because of how the state machine is set up, if I add this extra tick,
-  // the extended sta takes 1 more tick than it should.
-  // If I remove it, the extended ldx takes 1 less tick than it should.
   if (ctx.remainingTicks !== 0) {
     ctx.remainingTicks--;
     return null;
