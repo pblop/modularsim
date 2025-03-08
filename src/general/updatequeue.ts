aba29a09fa2abd53eeb3e94b6707507dfcf0197b
pblop <pablo@pabl.eu>
2025-03-08 15:15:55 +0100

    feat(updatequeue): allow passing a list of changes to the queue update fn
    for partial rendering!

--- src/general/updatequeue.ts
+++ src/general/updatequeue.ts
@@ -1,23 +1,32 @@
-export type UpdateFn = () => void | Promise<void>;
-export class UpdateQueue {
+export type UpdateFn<T> = (entries: T[]) => void | Promise<void>;
+export class UpdateQueue<T = never> {
   private updateQueued = false;
+  private entries: T[] = [];
 
-  constructor(private fn: UpdateFn) {
+  constructor(private fn: UpdateFn<T>) {
     this.updateQueued = false;
   }
 
-  queueUpdate = (): void => {
+  queueUpdate = (newEntry?: T): void => {
+    if (newEntry !== undefined) this.entries.push(newEntry);
+
     if (this.updateQueued) return;
     this.updateQueued = true;
 
     requestAnimationFrame(() => {
-      const res = this.fn();
+      const entries = this.entries.slice();
+      this.entries = [];
+
+      const res = this.fn(entries);
       if (res instanceof Promise) {
         res.then(() => {
           this.updateQueued = false;
+
+          if (this.entries.length > 0) this.queueUpdate();
         });
       } else {
         this.updateQueued = false;
+        if (this.entries.length > 0) this.queueUpdate();
       }
     });
   };
