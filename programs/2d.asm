6be516553a33fe17af29974949b2e4813f31d69a
pblop <pablo@pabl.eu>
2025-03-15 14:46:36 +0100

    asm: added testing program for 2d screen

---
+++ programs/2d.asm
@@ -0,0 +1,17 @@
+        .area PROG (ABS)
+
+        .org 0x100
+        .globl programa
+programa:
+	ldx 0xEF00
+  	lda 1
+bucle:
+        beq acabar
+        sta ,x+
+	adda 1
+        bra bucle
+acabar: clra
+        sta 0xFF01
+
+        .org 0xFFFE     ; Vector de RESET
+        .word programa
