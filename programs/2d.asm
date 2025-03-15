d3bca5fa26583bd9c79fda2d11fc11f267936779
pblop <pablo@pabl.eu>
2025-03-15 15:46:43 +0100

    asm: correct 2d screen testing program

--- programs/2d.asm
+++ programs/2d.asm
@@ -3,12 +3,12 @@
         .org 0x100
         .globl programa
 programa:
-	ldx 0xEF00
-  	lda 1
+	ldx #0xEF00
+  	lda #1
 bucle:
         beq acabar
         sta ,x+
-	adda 1
+	adda #0x01
         bra bucle
 acabar: clra
         sta 0xFF01
