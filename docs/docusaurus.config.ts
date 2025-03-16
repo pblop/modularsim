05330ba1887384075a8dcc69f4f998582d314544
pblop <pablo@pabl.eu>
2025-03-16 22:31:03 +0100

    docs: fix auto-generated api

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
       // Plugin for generating API documentation from TypeScript source code
       "docusaurus-plugin-typedoc",
       {
-        entryPoints: ["../src/main.ts"],
+        entryPoints: ["../src/main.ts", "../src/m6809/**/*.ts"],
         tsconfig: "../tsconfig.json",
       },
     ],
