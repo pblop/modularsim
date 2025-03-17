8142b2aed3c3deb9491cda784847fa667ac5400a
pblop <pablo@pabl.eu>
2025-03-17 01:54:26 +0100

    docs: fix deployment base url (it's not / on GitHub Pages)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: "https://your-docusaurus-site.example.com",
-  baseUrl: "/",
+  baseUrl: "/tfg/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
