eb24c25291a93a57981646af63825c706cae2ef7
pblop <pablo@pabl.eu>
2025-03-22 19:45:03 +0100

    feat(cpu): IRQ and NMI interrupts

--- src/m6809/hardware/states/fetch.ts
+++ src/m6809/hardware/states/fetch.ts
@@ -1,4 +1,4 @@
-import { truncate } from "../../../general/numbers.js";
+import { truncate, twosComplement } from "../../../general/numbers.js";
 import { INSTRUCTIONS } from "../../util/instructions.js";
 import type { CycleStartFn, CycleEndFn } from "../../util/state_machine";
 
@@ -21,10 +21,21 @@ const end: CycleEndFn<"fetch"> = (
 ) => {
   if (memoryPending) return null;
 
-  if (cpu.pendingNMI || cpu.pendingIRQ) {
+  // NOTE: Doing the change of state to interrupt within the fetch state means
+  // that, just like the real CPU, we will have fetched a memory byte, and will,
+  // therefore, discard it.
+  // That also means we need to decrement the PC by one, so that the correct
+  // value is pushed onto the stack in the event of an interrupt.
+  if (cpu.pendingNMI || cpu.pendingFIRQ || cpu.pendingIRQ) {
+    cpu.registers.pc = truncate(cpu.registers.pc + twosComplement(1, 16), 16);
+  }
+  // The priority order is NMI > FIRQ > IRQ.
+  if (cpu.pendingNMI) {
     return "irqnmi";
   } else if (cpu.pendingFIRQ) {
     return "firq";
+  } else if (cpu.pendingIRQ) {
+    return "irqnmi";
   }
 
   // Convert the opcode bytes (one or two) to a single u16 containing the
--- src/m6809/hardware/states/irqnmi.ts
+++ src/m6809/hardware/states/irqnmi.ts
@@ -1,10 +1,11 @@
 import { ConditionCodes } from "../../util/cpu_parts.js";
+import { IRQNMI_STACK_REGISTERS, pushRegisters } from "../../util/instructions/stack.js";
 import type { CycleStartFn, CycleEndFn } from "../../util/state_machine.js";
 
-const start: CycleStartFn<"irqnmi"> = (
-  { memoryPending, queryMemoryRead, config, cpu },
-  { ticksOnState, ctx },
-) => {
+const start: CycleStartFn<"irqnmi"> = (cpuInfo, stateInfo) => {
+  const { memoryPending, queryMemoryRead, config, cpu } = cpuInfo;
+  const { ctx, ticksOnState } = stateInfo;
+
   if (memoryPending) return;
 
   if (ticksOnState === 0) {
@@ -14,9 +15,36 @@ const start: CycleStartFn<"irqnmi"> = (
     } else if (cpu.pendingIRQ) {
       ctx.irq = true;
     }
-
-    // Fetch the interrupt vector.
-    queryMemoryRead(ctx.nmi ? config.nmiVector : config.irqVector, 2);
+  }
+  // (using figure 9 as a reference)
+  // m-1 is the last cycle of the previous instruction.
+  // m is the first cycle of the current instruction ("fetch" for us).
+  // m+1 is the second cycle of the current instruction ("irqnmi" cycle 0).
+  // that means, that our cycle counter is 1 cycle behind the timing on the
+  // figure.
+  // The Figure starts pushing the registers onto the stack on m+3, so we start
+  // on cycle 2
+  else if (ticksOnState >= 2 && ticksOnState <= 13) {
+    // We now push the registers onto the S stack, in order PC, U, Y, X, DP, B, A, CC,
+    // which is the same thing a PSHS PC,U,Y,X,DP,B,A,CC would do.
+    const pushedAllRegisters = pushRegisters(
+      cpuInfo,
+      "S",
+      IRQNMI_STACK_REGISTERS,
+      ctx,
+      "pushedRegisters",
+    );
+    if (!pushedAllRegisters && ticksOnState === 13) {
+      throw new Error("[cpu] Pushing registers due to interrupt took too long.");
+    }
+  } //else if (ticksOnState === 14) {
+  // cycle 14 (m+15) is a VMA cycle, which is not relevant for us.
+  //}
+  else if (ticksOnState === 15) {
+    // On cycle 15 (m+16), we fetch the first byte of the interrupt vector.
+    // On cycle 16 (m+17) we fetch the second byte (automatically done by queryMemoryRead).
+    const vector = ctx.nmi ? config.nmiVector : config.irqVector;
+    queryMemoryRead(vector, 2);
   }
 };
 
@@ -24,14 +52,24 @@ const end: CycleEndFn<"irqnmi"> = (
   { cpu, memoryPending, memoryAction, registers, commitRegisters, et },
   { ticksOnState, ctx },
 ) => {
-  // Clear the interrupt pending flag for the interrupt we're handling.
-  if (ctx.irq) {
-    cpu.pendingIRQ = false;
-  } else if (ctx.nmi) {
-    cpu.pendingNMI = false;
+  if (ticksOnState === 0) {
+    // Clear the interrupt pending flag for the interrupt we're handling.
+    if (ctx.irq) {
+      cpu.pendingIRQ = false;
+    } else if (ctx.nmi) {
+      cpu.pendingNMI = false;
+    }
+  } else if (ticksOnState === 16) {
+    // On cycle 16 (m+17), we should have the interrupt vector.
+    registers.pc = memoryAction!.valueRead;
+  } else if (ticksOnState === 17) {
+    // And finally, on cycle 17 (m+18), we have the last VMA cycle, and
+    // the last of the interrupt handling state.
+    commitRegisters();
+    return "fetch";
   }
 
-  return "fetch";
+  return "irqnmi";
 };
 
 export default { start, end };
