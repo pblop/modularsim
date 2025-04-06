6ac2f7fe793b056f5b4b1dc9ea8ac2be28f15cbc
pblop <pablo@pabl.eu>
2025-04-06 23:43:08 +0200

    docs: add src/general again (correctly), and fix acorn crashing again (correctly)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
       // Plugin for generating API documentation from TypeScript source code
       "docusaurus-plugin-typedoc",
       {
-        entryPoints: ["../src/main.ts", "../src/m6809/**/*.ts", "../src/general/**/*.js"],
+        entryPoints: ["../src/main.ts", "../src/m6809/**/*.ts", "../src/general/**/*.ts"],
         tsconfig: "../tsconfig.json",
         formatWithPrettier: true,
       },
