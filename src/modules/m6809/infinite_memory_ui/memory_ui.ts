37fb5fc53a70a95a283bbdec35967e5de1fa91f1
pblop <pablo@pabl.eu>
2025-04-19 01:49:32 +0200

    feat(infmem): correctly refresh the UI in new system

--- src/modules/m6809/infinite_memory_ui/memory_ui.ts
+++ src/modules/m6809/infinite_memory_ui/memory_ui.ts
@@ -52,7 +52,7 @@ class MemoryUI implements IModule {
   language!: string;
   localeStrings!: typeof MemoryUIStrings.en;
 
-  updateQueue: UpdateQueue<UpdateQueueElement>;
+  updateQueue: UpdateQueue;
 
   getModuleDeclaration(): ModuleDeclaration {
     return {
@@ -125,50 +125,41 @@ class MemoryUI implements IModule {
     if (register !== "pc") return;
     if (!this.panel || !this.memoryTable) return;
 
-    if (pc < this.config.start || pc >= this.config.start + this.config.size) return;
-
-    this.updateQueue.queueUpdate({ type: "pc", address: pc });
+    this.pc = pc;
+    this.updateQueue.queueUpdate();
   };
   onMemoryRead = (address: number): void => {
-    if (!this.panel) return;
-    if (!this.memoryTable) return;
+    if (!this.panel || !this.memoryTable) return;
 
-    if (address < this.config.start || address >= this.config.start + this.config.size) return;
-
-    this.updateQueue.queueUpdate({ type: "read", address });
+    this.lastMemoryRead = address;
+    this.updateQueue.queueUpdate();
   };
   onMemoryWrite = (address: number): void => {
-    if (!this.panel) return;
-    if (!this.memoryTable) return;
+    if (!this.panel || !this.memoryTable) return;
 
-    if (address < this.config.start || address >= this.config.start + this.config.size) return;
-
-    this.updateQueue.queueUpdate({ type: "write", address });
+    this.lastMemoryWrite = address;
+    this.updateQueue.queueUpdate();
   };
 
   onMemoryBulkWriteResult = (dataStart: number, data: Uint8Array): void => {
-    if (!this.panel) return;
-    if (!this.memoryTable) return;
+    if (!this.panel || !this.memoryTable) return;
 
     this.memory.set(data, dataStart);
-
-    this.updateQueue.queueUpdate({ type: "bulk_write", address: dataStart, data });
+    this.updateQueue.queueUpdate();
   };
   onMemoryWriteResult = (address: number, data: number): void => {
-    if (!this.panel) return;
-    if (!this.memoryTable) return;
+    if (!this.panel || !this.memoryTable) return;
 
     this.memory[address] = data;
-
-    this.updateQueue.queueUpdate({ type: "write_result", address, data });
+    this.updateQueue.queueUpdate();
   };
   onReset = (): void => {
-    if (!this.panel) return;
-    if (!this.memoryTable) return;
+    if (!this.panel || !this.memoryTable) return;
 
-    this.updateQueue.queueUpdate({ type: "pc", address: undefined });
-    this.updateQueue.queueUpdate({ type: "read", address: undefined });
-    this.updateQueue.queueUpdate({ type: "write", address: undefined });
+    this.pc = undefined;
+    this.lastMemoryRead = undefined;
+    this.lastMemoryWrite = undefined;
+    this.updateQueue.queueUpdate();
   };
 
   createMemoryUI(): void {
@@ -219,97 +210,27 @@ class MemoryUI implements IModule {
       for (let j = 0; j < 0x10; j++) {
         const cell = cells[j];
         const address = startAddress + j;
-        if (address < endAddress) cell.textContent = this.formatMemoryData(this.memory[address]);
-        else cell.textContent = "";
+        if (address < endAddress) {
+          cell.textContent = this.formatMemoryData(this.memory[address]);
+          let className = "contrast-color";
+          if (this.lastMemoryRead === address) className += " read-highlight";
+          if (this.lastMemoryWrite === address) className += " write-highlight";
+          if (this.pc === address) className += " pc-highlight";
+          cell.className = className;
+        } else {
+          cell.textContent = "";
+          cell.className = "";
+        }
       }
 
       return node;
     }
   };
 
-  refreshUI = async (queue: UpdateQueueElement[]) => {
-    // We only need the last update of the read/write/pc types.
-    let hasRead = false;
-    let hasWrite = false;
-    let hasPC = false;
-
-    for (let i = queue.length - 1; i >= 0; i--) {
-      const type = queue[i].type;
-      if (type === "read") {
-        if (hasRead) queue.splice(i, 1);
-        else hasRead = true;
-      } else if (type === "write") {
-        if (hasWrite) queue.splice(i, 1);
-        else hasWrite = true;
-      } else if (type === "pc") {
-        if (hasPC) queue.splice(i, 1);
-        else hasPC = true;
-      }
-    }
-
-    for (const update of queue) {
-      switch (update.type) {
-        case "read":
-          this.updateHighlight(update.address, "read-highlight");
-          break;
-        case "write":
-          this.updateHighlight(update.address, "write-highlight");
-          break;
-        case "write_result":
-          this.updateMemory(update.address, update.data);
-          break;
-        case "pc":
-          this.updateHighlight(update.address, "pc-highlight");
-          break;
-        case "bulk_write":
-          this.bulkUpdateMemory(update.address, update.data);
-          break;
-      }
-    }
+  refreshUI = () => {
+    if (!this.memoryTable) return;
+    this.memoryTable.updateContents();
   };
-
-  updateHighlight(address: number | undefined, highlightClassName: string): void {
-    if (!this.panel || !this.memoryTable) return;
-
-    // Clear the previous address highlight.
-    // this.panel.querySelector(`.${highlightClassName}`)?.classList.remove(highlightClassName);
-
-    if (address === undefined) return;
-
-    // Find the cell corresponding to the new address and highlight it.
-    // this.panel.querySelector(`.byte-${address}`)?.classList.add(highlightClassName);
-  }
-  bulkUpdateMemory(dataStart: number, data: Uint8Array): void {
-    if (!this.panel) return;
-
-    const dataEnd = dataStart + data.length;
-
-    // If the start of the bulk write is before the start of the memory table,
-    // we will start from the start of the memory table.
-    // Otherwise, if the start of the bulk write is after the start of the
-    // memory table, we will start from the start of the bulk write.
-    const initial = Math.max(dataStart, this.config.start);
-
-    for (
-      let address = initial;
-      // We will iterate until the end of the memory table or the end of the
-      // bulk write, whichever comes first.
-      address < this.config.start + this.config.size && address < dataEnd;
-      address++
-    ) {
-      // const cell = this.panel.querySelector(`.byte-${address}`);
-      // if (!cell) continue;
-      // cell.textContent = this.formatMemoryData(data[address - dataStart]);
-    }
-  }
-  updateMemory(address: number, data: number): void {
-    if (!this.panel) return;
-
-    // const cell = this.panel.querySelector(`.byte-${address}`);
-    // if (!cell) return;
-
-    // cell.textContent = this.formatMemoryData(data);
-  }
 }
 
 export default MemoryUI;
